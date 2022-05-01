b97913f21f76991ae84d57cd86dd33c64203f164
GerardoM3 <gerardomonroy13@gmail.com>
2022-04-30 22:12:04 -0600

    Primer commit: Creación de un método usando git desde NetBeans (Gerardo Monroy)

--- Ejercicio_3/src/demo_crud/demo_crud.java
+++ Ejercicio_3/src/demo_crud/demo_crud.java
@@ -2,5 +2,12 @@
 package demo_crud;
 
 public class demo_crud {
-    
+    public static void main(String[] args) {
+        GerardoMonroy();
+    }
+    //Primer commit: Creación de un método cada miembro del equipo, y mandarlo a llamar desde el método main.
+    public static void GerardoMonroy(){
+        
+    }
 }
+
